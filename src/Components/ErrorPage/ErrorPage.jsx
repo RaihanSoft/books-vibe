578de2fdfb2dc27d8a9e38f98e68ec0644b47534
Raihan <aboraihan14@gmail.com>
2024-10-31 20:31:28 +0600

    navbar and footer addedd

---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+  return (
+    <div className=" flex items-center justify-center flex-col">
+        <img className="" src="https://i0.wp.com/learn.onemonth.com/wp-content/uploads/2017/08/1-10.png?fit=845%2C503&ssl=1" alt="" />
+      
+    </div>
+  )
+}
+
+export default ErrorPage
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Components/Root/Root';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
+import Home from './Components/Home/Home';
+import DashBoard from './Components/DashBoard/DashBoard';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }, {
+        path: '/dashboard',
+        element: <DashBoard />
+      },
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
